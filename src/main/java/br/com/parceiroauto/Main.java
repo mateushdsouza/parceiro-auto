42338dc52819f7300bfe28389941ae784b8593ff
Gustavo Vinícius Milkiewicz Bezerra <gustamilkbena2000@gmail.com>
2026-03-02 10:17:12 -0300

    ClasseUsuario

--- src/main/java/br/com/parceiroauto/Main.java
+++ src/main/java/br/com/parceiroauto/Main.java
@@ -2,6 +2,6 @@ package br.com.parceiroauto;
 
 public class Main {
     public static void main(String[] args) {
-        int test = 10;
+
     }
 }
--- src/main/java/br/com/parceiroauto/model/Reports.java
+++
@@ -1,4 +0,0 @@
-package br.com.parceiroauto.model;
-
-public class Reports {
-}
