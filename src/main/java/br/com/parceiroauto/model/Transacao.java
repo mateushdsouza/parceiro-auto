8c4ea9c2f7913ad4aa073e584798a74597c1c393
Kaiser <glhrmkaiser@gmail.com>
2026-03-05 22:06:04 -0300

    finalizado parde de entrada e saida

--- src/main/java/br/com/parceiroauto/model/Transacao.java
+++ src/main/java/br/com/parceiroauto/model/Transacao.java
@@ -3,20 +3,23 @@ package br.com.parceiroauto.model;
 import java.util.Date;
 
 public class Transacao {
-    private String tipo;
+    private TipoTransacao tipo;
     private String descricao;
     private double valor;
     private Date data;
+    private FormaDeTransacao forma;
 
-    public Transacao(String tipo, String descricao, double valor, Date data) {
+    public Transacao(TipoTransacao tipo, String descricao, double valor, FormaDeTransacao forma) {
         this.tipo = tipo;
         this.descricao = descricao;
         this.valor = valor;
         this.data = new Date();
+        this.forma = forma;
     }
 
-    public String getTipo() { return tipo; }
+    public TipoTransacao getTipo() { return tipo; }
     public String getDescricao() { return descricao; }
     public double getValor() { return valor; }
     public Date getData() { return data; }
+    public FormaDeTransacao getForma() {return forma;}
 }
