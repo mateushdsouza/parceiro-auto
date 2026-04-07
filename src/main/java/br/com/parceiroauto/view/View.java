9ec44e5bdf05ac7d1d39c725381bd5180998a86d
Kaiser <glhrmkaiser@gmail.com>
2026-04-07 20:29:03 -0300

    fazer no pc

--- src/main/java/br/com/parceiroauto/view/View.java
+++ src/main/java/br/com/parceiroauto/view/View.java
@@ -1,5 +1,34 @@
 package br.com.parceiroauto.view;
 
+import br.com.parceiroauto.entity.User;
+import br.com.parceiroauto.repository.UserRepository;
+import br.com.parceiroauto.service.UserService;
+
+import java.util.Scanner;
+
 public class View {
-    //A refatorar
+    public static User menuLogin() {
+        UserService userService = new UserService(new UserRepository());
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("1 - Fazer login");
+        System.out.println("2 - Fazer cadastro");
+        System.out.println("3 - Sair");
+        int opcaoMenuLogin = sc.nextInt();
+
+        do {
+            switch (opcaoMenuLogin) {
+                case 1:
+
+                    break;
+
+                case 2:
+                    break;
+
+                default:
+                    System.out.println("Digite uma opcao valida!");
+                    break;
+            }
+        } while (opcaoMenuLogin != 3);
+    }
 }
