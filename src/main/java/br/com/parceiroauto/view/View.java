12e10567d47ed666ee059462e40aac291aafaeb5
Kaiser <glhrmkaiser@gmail.com>
2026-04-07 19:54:20 -0300

    fix: deletando view antigo

--- src/main/java/br/com/parceiroauto/view/View.java
+++ src/main/java/br/com/parceiroauto/view/View.java
@@ -1,10 +1,5 @@
 package br.com.parceiroauto.view;
 
-import br.com.parceiroauto.entity.*;
-import br.com.parceiroauto.repository.UserRepository;
-
-import java.util.Scanner;
-
 public class View {
     //A refatorar
 }
