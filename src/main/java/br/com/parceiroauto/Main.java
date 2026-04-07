a22ddb3affc8815e7658c25a2a1aa88a449287c0
Kaiser <glhrmkaiser@gmail.com>
2026-04-06 21:04:24 -0300

    feat: versionamento de banco de dados

--- src/main/java/br/com/parceiroauto/Main.java
+++ src/main/java/br/com/parceiroauto/Main.java
@@ -1,8 +1,11 @@
 package br.com.parceiroauto;
+import br.com.parceiroauto.confg.FlyWayconfg;
 import br.com.parceiroauto.view.View;
 
 public class Main {
     public static void main(String[] args) {
         View.menuPrincipal();
+        FlyWayconfg.migrate();
+
     }
 }
