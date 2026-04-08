9f807ef0258f41941bf9e65933db6faa5a3f891e
Kaiser <glhrmkaiser@gmail.com>
2026-04-07 21:44:05 -0300

    arrumado questao do jpa

--- src/main/java/br/com/parceiroauto/Main.java
+++ src/main/java/br/com/parceiroauto/Main.java
@@ -2,8 +2,16 @@ package br.com.parceiroauto;
 import br.com.parceiroauto.confg.FlyWayconfg;
 import br.com.parceiroauto.view.View;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+
 public class Main {
     public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("parceiroautoPU");
+        EntityManager em = emf.createEntityManager();
+
         View.menuPrincipal();
         FlyWayconfg.migrate();
 
