b2889a78b5faeeb218b1d11a917fe38dd32ca11a
Kaiser <glhrmkaiser@gmail.com>
2026-04-07 21:23:58 -0300

    feito login e cadastro

--- src/main/java/br/com/parceiroauto/view/View.java
+++ src/main/java/br/com/parceiroauto/view/View.java
@@ -7,28 +7,72 @@ import br.com.parceiroauto.service.UserService;
 import java.util.Scanner;
 
 public class View {
+
     public static User menuLogin() {
         UserService userService = new UserService(new UserRepository());
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("1 - Fazer login");
-        System.out.println("2 - Fazer cadastro");
-        System.out.println("3 - Sair");
-        int opcaoMenuLogin = sc.nextInt();
+        int opcaoMenuLogin;
 
         do {
+            System.out.println("1 - Fazer login");
+            System.out.println("2 - Fazer cadastro");
+            System.out.println("3 - Sair");
+            opcaoMenuLogin = sc.nextInt();
+
             switch (opcaoMenuLogin) {
                 case 1:
+                    System.out.println("Digite seu login:");
+                    String login = sc.next();
+
+                    System.out.println("Digite sua senha:");
+                    String senha = sc.next();
+
+                    try {
+                        User user = userService.authenticate(login, senha);
+
+                        if (user != null) {
+                            System.out.println("Login realizado com sucesso!");
+                            return user;
+                        } else {
+                            System.out.println("Login ou senha incorretos!");
+                        }
+
+                    } catch (IllegalArgumentException e) {
+                        System.out.println(e.getMessage());
+                    }
 
                     break;
 
                 case 2:
+                    System.out.println("Digite um login:");
+                    String novoLogin = sc.next();
+
+                    System.out.println("Digite uma senha:");
+                    String novaSenha = sc.next();
+
+                    try {
+                        User novoUser = userService.createUser(novoLogin, novaSenha);
+
+                        System.out.println("Usuario cadastrado com sucesso!");
+                        System.out.println("Bem-vindo, " + novoUser.getLogin() + "!");
+
+                        return novoUser;
+
+                    } catch (IllegalArgumentException e) {
+                        System.out.println(e.getMessage());
+                    }
+
                     break;
 
+                case 3:
+                    System.out.println("Saindo...");
+                    return null;
+
                 default:
-                    System.out.println("Digite uma opcao valida!");
-                    break;
+                    System.out.println("Digite uma opção válida!");
             }
-        } while (opcaoMenuLogin != 3);
+
+        } while (true);
     }
 }
