0804e52f497b9e18060ce92574f364fd1962f91c
Kaiser <glhrmkaiser@gmail.com>
2026-04-08 22:24:13 -0300

    finalizado menu da empresas

--- src/main/java/br/com/parceiroauto/Main.java
+++ src/main/java/br/com/parceiroauto/Main.java
@@ -3,14 +3,11 @@ import br.com.parceiroauto.confg.FlyWayconfg;
 import br.com.parceiroauto.confg.JPAUtil;
 import br.com.parceiroauto.view.View;
 
-import jakarta.persistence.EntityManager;
-
 public class Main {
     public static void main(String[] args) {
         FlyWayconfg.migrate();
-        EntityManager em = JPAUtil.getEntityManager();
-
-        View.menuLogin();
+        View.start();
+        JPAUtil.close();
 
     }
 }
