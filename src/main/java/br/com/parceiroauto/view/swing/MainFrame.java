437c01bef4f710b9871f41c4468a0b162babedea
mateus <teushdsouza@gmail.com>
2026-05-11 12:04:30 -0300

    Rename HomeFrame to MainFrame

--- src/main/java/br/com/parceiroauto/view/swing/MainFrame.java
+++ src/main/java/br/com/parceiroauto/view/swing/MainFrame.java
@@ -2,9 +2,9 @@ package br.com.parceiroauto.view.swing;
 
 import javax.swing.*;
 
-public class HomeFrame extends JFrame {
+public class MainFrame extends JFrame {
 
-    public HomeFrame() {
+    public MainFrame() {
         setSize(900, 600);
         JLabel lblWelcome = new JLabel("Seja bem vindo");
 
